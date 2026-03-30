227c2affc31ede3712f233fa1f36529f2e600ee2
adarshkrishna-OG <krishnaadarsh025@gmail.com>
2026-03-30 11:58:44 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,14 @@
+package graphics;
+
+public class Rectangle implements Shape {
+    double l, b;
+
+    public Rectangle(double l, double b) {
+        this.l = l;
+        this.b = b;
+    }
+
+    public double area() {
+        return l * b;
+    }
+}
---
+++ Square.java
@@ -0,0 +1,13 @@
+package graphics;
+
+public class Square implements Shape {
+    double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double area() {
+        return side * side;
+    }
+}
